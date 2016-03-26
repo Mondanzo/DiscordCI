3e249edf2225077bc2bb7e34483a8687dcbfb977
Andrew Blakely <andrew.blakely@ymail.com>
2016-03-26 17:11:29 -0500

    Added sln

---
+++ src/DiscordCI.Application/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordCI.Application
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
